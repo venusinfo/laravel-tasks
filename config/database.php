ca04499acee786b567586ecfc6c5609371c42295
Venus Informatics <40406718+venusinfo@users.noreply.github.com>
2023-07-04 08:58:36 +1200

    changes to rdis config with AZURE config variables

--- config/database.php
+++ config/database.php
@@ -130,11 +130,12 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'password' => env('AZURE_REDIS_PASSWORD'),
+            'port' => env('AZURE_REDIS_PORT', '6379'),
+            'database' => env('AZURE_REDIS_DATABASE', '0'),
+            'scheme' => 'tls',
         ],
 
         'cache' => [
